5a7240221e0dd38f0df5435e36722a571c8444f7
Alefh Sousa <alefh.sousa@gmail.com>
2020-09-22 22:04:29 -0300

    adicionando hystrix no exemplo do api gateway

--- fj33-api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
+++ fj33-api-gateway/src/main/java/br/com/caelum/apigateway/DistanciaRestClient.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 class DistanciaRestClient {
 
@@ -17,6 +19,7 @@ class DistanciaRestClient {
 		this.distanciaServiceUrl = distanciaServiceUrl;
 	}
 
+	@HystrixCommand
 	Map<String, Object> porCepEId(String cep, Long restauranteId) {
 		String url = distanciaServiceUrl + "/restaurantes/" + cep + "/restaurante/" + restauranteId;
 		return restTemplate.getForObject(url, Map.class);
